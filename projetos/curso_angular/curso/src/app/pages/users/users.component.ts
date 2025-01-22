f8415eba027789043ea3c4e8f27929d47a1cbe95
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2025-01-22 14:58:35 -0400

    Incluindo usuários com Reactive Forms

--- projetos/curso_angular/curso/src/app/pages/users/users.component.ts
+++ projetos/curso_angular/curso/src/app/pages/users/users.component.ts
@@ -29,8 +29,9 @@ export class UsersComponent implements OnInit {
 
   SubmitForm(){
     if(this.userForm.valid){
-      console.log(this.userForm.value);
-    }
+      this.users.push(this.userForm.value)
+      this.userForm.reset()
+    };
   }
 
   users: User[] = [
